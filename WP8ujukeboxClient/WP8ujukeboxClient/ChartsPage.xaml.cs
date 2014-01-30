fc0f020ede4fb547ae5b43b242dd8543bc9058d5
tommydelacey@gmail.com <tommydelacey@gmail.com@51be613e-00c5-3753-6cca-790d0c3fb8ea>
2014-01-30 15:09:29 +0000

    All New Client. PUT now working. Fully working client.

---
+++ WP8ujukeboxClient/WP8ujukeboxClient/ChartsPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using WP8ujukeboxClient.ViewModels;
+
+namespace WP8ujukeboxClient
+{
+    public partial class ChartsPage : PhoneApplicationPage
+    {
+        public ChartsPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the LongListSelector control to the sample data
+            DataContext = App.ViewModel;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        // Handle selection changed on LongListSelector
+        private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If selected item is null (no selection) do nothing
+            if (MainLongListSelector.SelectedItem == null)
+                return;
+
+            //Navigate to the new page
+            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (MainLongListSelector.SelectedItem as ItemViewModel).ID + "&fromChart=true", UriKind.Relative));
+
+            // Reset selected item to null (no selection)
+            MainLongListSelector.SelectedItem = null;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+    }
+}
---
+++ WP8ujukeboxClient/WP8ujukeboxClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WP8ujukeboxClient.Resources;
+using System.Linq;
+
+namespace WP8ujukeboxClient.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        // URI for RESTful service (implemented using Web API)
+        private const String serviceURI = "http://ujukebox.azurewebsites.net/api/ujukeapi";
+
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+            this.Items2 = new ObservableCollection<ItemViewModel>();
+            
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+        public ObservableCollection<ItemViewModel> Items2 { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sample property that returns a localized string
+        /// </summary>
+        public string LocalizedSampleProperty
+        {
+            get
+            {
+                return AppResources.SampleProperty;
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public async void LoadData()
+        {
+                HttpClient client = new HttpClient();
+
+                // base URL for API Controller i.e. RESTFul service
+                client.BaseAddress = new Uri("http://ujukebox.azurewebsites.net/");       
+
+                // add an Accept header for JSON
+                client.DefaultRequestHeaders.
+                Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/ujukeapi");
+                
+                // read result     
+                var lists = await response.Content.ReadAsAsync<IEnumerable<Track>>();
+                IEnumerable<Track> listings = lists.OrderBy(list => list.Title);
+
+            //index id for list of items    
+            int newid = 0;
+
+            //to hold real item id from db
+            string getId = "";
+
+            foreach (var listing in listings)
+            {
+                //get the original id and save to getid
+                getId = listing.ID;
+                              
+                //Real to pass the realid 
+                this.Items.Add(new ItemViewModel() { RealID = getId, ID = newid.ToString(), LineOne = listing.Title, LineTwo = listing.Artist, LineThree = listing.Genre, LineFour = listing.Vote });
+                
+                //newid is used to set ID to 0 - the index of the item in the displayed list
+                newid++;
+            }
+
+            //to set a model sorted by vote
+            IEnumerable<Track> listings2 = lists.OrderByDescending(list => list.Vote);
+            
+            //set chart position to start at 1
+            int position = 1;
+            int newid2 = 0;
+            string getId2 = "";
+            
+            foreach (var listing in listings2)
+            {
+                getId2 = listing.ID;
+
+                this.Items2.Add(new ItemViewModel() { RealID = getId2, ID = newid2.ToString(), LineOne = listing.Title, LineTwo = listing.Artist, LineThree = listing.Genre, LineFour = listing.Vote, LineFive = position.ToString() });
+                position++;
+                newid2++;
+            }
+                   
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
