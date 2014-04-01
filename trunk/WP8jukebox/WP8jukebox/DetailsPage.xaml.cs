1ab9d189c9a8c285081b53f2763230360efa0d99
tommydelacey@gmail.com <tommydelacey@gmail.com@51be613e-00c5-3753-6cca-790d0c3fb8ea>
2014-04-01 15:09:16 +0000

    Button navigation working Passing data ok

--- trunk/WP8jukebox/WP8jukebox/DetailsPage.xaml.cs
+++ trunk/WP8jukebox/WP8jukebox/DetailsPage.xaml.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using WP8jukebox.Resources;
+using WP8jukebox.ViewModels;
 
 namespace WP8jukebox
 {
     public partial class DetailsPage : PhoneApplicationPage
     {
-      
-        
+        string fromChart = "";
         // Constructor
         public DetailsPage()
         {
@@ -31,39 +34,107 @@ namespace WP8jukebox
             string getGenre = "";
             string getTrack = "";
 
-           
+            //hides vote acknowledgement popup 
+            Text1.Visibility = Visibility.Collapsed;
+        
+
+
+            getGenre = NavigationContext.QueryString["getGenre"];
+            getVenue = NavigationContext.QueryString["getVenue"];
+            //getVenue = NavigationContext.QueryString["fromChart"];
+
+
+
+            if (NavigationContext.QueryString.TryGetValue("fromChart", out fromChart))
+            {
+            }
+
+
+
+
             
-                if (DataContext == null)
+            if (DataContext == null)
                 {
                     string selectedIndex = "";
                     if (NavigationContext.QueryString.TryGetValue("selectedItem", out selectedIndex))
                     {
                         int index = int.Parse(selectedIndex);
-                        //DataContext = App.ViewModel.Items3[index];
+                        DataContext = App.ViewModel.Items3[index];
                         getTrack = App.ViewModel.Items3[index].LineOne;
 
-                        getGenre = NavigationContext.QueryString["getGenre"];
-                        getVenue = NavigationContext.QueryString["getVenue"];
+                        
                        
                     }
                 }
             
         }
+        //make the vote
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //msg to acknowledge vote
+            Text1.Visibility = Visibility.Visible;
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+            ItemViewModel tr = (ItemViewModel)this.DataContext;
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+            //get the values to be sent to db to facilitate update PUT to db
+            //int id = Convert.ToInt32(getreal);
+            string title = tr.LineOne;
+            string artist = tr.LineTwo;
+            string genre = tr.LineThree;
+            
+           // int vote = Convert.ToInt32(tr.LineFour);
 
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+            //increment the vote number
+            //vote++;
+            //increment the displayed vote number
+            //tr.LineFour++;
+
+            // base URL for API Controller i.e. RESTFul service
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://ujuke.azurewebsites.net/");
+
+            // add an Accept header for JSON
+            client.DefaultRequestHeaders.
+            Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/ujukeapi");
+
+            //getreal sets the db id row to the correct value
+            //Track newListing = new Track { ID = getreal, Title = title, Artist = artist, Genre = genre, Vote = vote };
+
+            // update by Put to /api/ujukeapi a listing serialised in request body
+            //the +id is added to the url to address the correct row in the db
+           // response = await client.PutAsJsonAsync("api/ujukeapi/" + id, newListing);
+
+            //if PUT fails
+            if (!response.IsSuccessStatusCode)
+            {
+                //TODO
+                //Uri newStockUri = response.Headers.Location;
+                //Console.WriteLine(response.StatusCode + " " + response.ReasonPhrase);
+            }
+
+            //delay the page navigation so user can see vote acknowledgement                      
+            Thread.Sleep(1000);
+
+            //force a reload of the model so all pages have correct data
+            App.ViewModel = null;
+
+            //navigated to from chart page , then navigate back to that page
+            if (fromChart == "true")
+            {
+                NavigationService.Navigate(new Uri("/ChartPage.xaml", UriKind.Relative));
+            }
+            else
+            {
+                //else back to playlist page            
+                NavigationService.Navigate(new Uri("/PlaylistPage.xaml", UriKind.Relative));
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
