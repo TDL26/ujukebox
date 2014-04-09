75f966daa57bba4e3b1d3c8895b67ead5d72f15f
tommydelacey@gmail.com <tommydelacey@gmail.com@51be613e-00c5-3753-6cca-790d0c3fb8ea>
2014-04-09 16:17:37 +0000

    new version with redone navigation

---
+++ WP8jukeboxAPRv7/WP8jukebox/Track.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WP8jukebox
+{
+    class Track
+    {
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "artist")]
+        public string Artist { get; set; }
+
+        [JsonProperty(PropertyName = "genre")]
+        public string Genre { get; set; }
+
+        [JsonProperty(PropertyName = "vote")]
+        public int Vote { get; set; }
+
+        public string Club92 { get; set; }
+        public string Coppers { get; set; }
+        public string Buskers { get; set; }
+
+    }
+}
