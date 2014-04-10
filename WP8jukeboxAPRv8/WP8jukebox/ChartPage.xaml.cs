8c450f8f27957a592b560c9fafc5dd5303c8818c
tommydelacey@gmail.com <tommydelacey@gmail.com@51be613e-00c5-3753-6cca-790d0c3fb8ea>
2014-04-10 12:29:19 +0000

    Redone all comments - Chart Page UI redone with columns TODO genre page

---
+++ WP8jukeboxAPRv8/WP8jukebox/ChartPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WP8jukebox.ViewModels;
+
+namespace WP8jukebox
+{
+    public partial class ChartPage : PhoneApplicationPage
+    {
+         string getVenue = "";
+         public static string venueBox { get; set; }
+
+        public ChartPage()
+        {
+            InitializeComponent();
+                       
+            // Set the data context of the LongListSelector control to the sample data
+            DataContext = App.ViewModel;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (NavigationContext.QueryString.ContainsKey("getVenue"))
+            {
+                getVenue = NavigationContext.QueryString["getVenue"];
+                venueBox = getVenue;
+                textBox1.Text = venueBox;
+            }
+
+            //App.ViewModel = null;
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadChartData();
+            }
+        }
+        
+        // Handle selection changed on LongListSelector
+        private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If selected item is null (no selection) do nothing
+            if (MainLongListSelector.SelectedItem == null)
+                return;
+
+            // Navigate to the new page
+            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (MainLongListSelector.SelectedItem as ItemViewModel).ID+"&getVenue="+getVenue+"&fromChart=true", UriKind.Relative));
+
+            // Reset selected item to null (no selection)
+            MainLongListSelector.SelectedItem = null;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/PlaylistPage.xaml" + "?getVenue=" + getVenue  + "&fromChart=true", UriKind.Relative));
+        }
+    }
+}
