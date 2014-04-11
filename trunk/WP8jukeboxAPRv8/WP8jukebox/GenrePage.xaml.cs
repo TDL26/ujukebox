1f8ae36055e8436bfb45dfd5e92968620fb1362e
tommydelacey@gmail.com <tommydelacey@gmail.com@51be613e-00c5-3753-6cca-790d0c3fb8ea>
2014-04-11 10:31:28 +0000

    Removed Usings Formatted document code analysis ok

--- trunk/WP8jukeboxAPRv8/WP8jukebox/GenrePage.xaml.cs
+++ trunk/WP8jukeboxAPRv8/WP8jukebox/GenrePage.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using WP8jukebox.ViewModels;
 
 namespace WP8jukebox
 {
